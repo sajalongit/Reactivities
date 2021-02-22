be2599c7aaf44953b215b2c6cff2440638ba8686
Sajal Gupta <personal@sajalgupta.in>
2021-02-22 19:46:25 +1100

    End of section 8

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,24 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import LoadingComponent from './LoadingComponent';
-import { useStore } from '../stores/Store';
 import { observer } from 'mobx-react-lite';
+import HomePage from '../../features/home/HomePage';
+import { Route, useLocation } from 'react-router-dom';
+import ActivityForm from '../../features/activities/form/ActivityForm';
+import ActivityDetails from '../../features/activities/details/ActivityDetails';
 
 function App() {
-  const {activityStore} = useStore();
-  useEffect(() => {
-   activityStore.loadActivities();
-  }, [activityStore])
+  const location = useLocation();
 
-  if(activityStore.loadingInitial) return <LoadingComponent content="Loading App" inverted={true} />
   return (
     <>
-      <NavBar />
-      <Container style={{marginTop: '7em'}}>
-        <ActivityDashboard />
-      </Container>
+    <Route exact path='/' component={HomePage} />
+    <Route 
+      path={'/(.+)'}
+      render={() => (
+        <>
+          <NavBar />
+          <Container style={{marginTop: '7em'}}>
+            <Route exact path='/activities' component={ActivityDashboard} />
+            <Route path='/activities/:id' component={ActivityDetails} />
+            <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+          </Container>
+        </>
+      )}
+    />
     </>
   );
 }
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/Store';
 
@@ -28,7 +29,7 @@ export default observer(function ActivityList(){
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue' />
                                 <Button 
                                 name = {activity.id}
                                 loading={submitting && target == activity.id} onClick={(e) => handleActivityDelete(e, activity.id)} floated='right' content='Delete' color='red' />
