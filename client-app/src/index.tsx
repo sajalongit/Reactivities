097ee5d46c31a53e081f8354cf1706ff97fc2888
Sajal Gupta <personal@sajalgupta.in>
2021-02-25 01:15:44 +1100

    End of section 10

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,17 +3,22 @@ import ReactDOM from 'react-dom';
 import './app/layout/styles.css';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css'
+import 'react-toastify/dist/ReactToastify.min.css'
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 
 import { store, StoreContext } from './app/stores/Store';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
+
+import {createBrowserHistory} from 'history';
+
+export const history= createBrowserHistory();
 
 ReactDOM.render(
   <StoreContext.Provider value={store}>
-    <BrowserRouter>
+    <Router history={history}>
       <App />
-    </BrowserRouter>
+    </Router>
   </StoreContext.Provider>,  
   document.getElementById('root')
 );
